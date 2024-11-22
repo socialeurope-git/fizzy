43fa91fc05b6485070643c71efafdb3af2056fa4
Jason Zimdars <jz@37signals.com>
2024-11-22 14:19:50 -0600

    Needs a little structure

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
   def comment_tag(comment, &)
-    tag.div id: dom_id(comment), class: "comment flex align-start full-width",
+    tag.div id: dom_id(comment), class: "comment flex-inline align-start full-width",
       data: { creator_id: comment.creator_id, created_by_current_user_target: "creation" }, &
   end
 
